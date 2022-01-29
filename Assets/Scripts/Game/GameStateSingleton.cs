cd7d415fe91e660b387d096e82aa51d7c93b59cc
Liech <nicolas_harttig@hotmail.de>
2022-01-29 18:02:30 +0100

    added energybar

--- Assets/Scripts/Game/GameStateSingleton.cs
+++ Assets/Scripts/Game/GameStateSingleton.cs
@@ -3,23 +3,30 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class GameStateSingleton : MonoBehaviour
-{
+public class GameStateSingleton : MonoBehaviour {
   public static GameStateSingleton instance;
 
   public List<PlayerInfo> Players = new List<PlayerInfo>();
   public GameObject       GUI = null;
 
-
+  
   private void Start() {
     instance=this;
     Players.Add(new PlayerInfo());
-    if (GUI)
-      GUI = Instantiate(GUI);
+    if(GUI) {
+      GUI=Instantiate(GUI);
+    }
   }
 
   private void Update() {
     if(GUI)
-      GUI.transform.Find("HitCount").GetComponent<Text>().text = "Hits: " + Players[0].Deaths.ToString();
+      for(int i = 0;i<Players.Count;i++) {
+        GUI.transform.GetChild(i).Find("HitCount").GetComponent<Text>().text="Hits: "+Players[0].Deaths.ToString();
+
+        var rect = GUI.transform.GetChild(i).Find("EnergybarBackground").Find("EnergybarCurrent").GetComponent<RectTransform>();
+        var background = GUI.transform.GetChild(i).Find("EnergybarBackground").GetComponent<RectTransform>();
+        float value = 0.5f;
+        rect.sizeDelta=new Vector2(background.sizeDelta.x * value, background.sizeDelta.y);        
+      }
   }
 }
