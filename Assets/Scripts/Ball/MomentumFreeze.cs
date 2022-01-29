4a9bcd961ab74f68a21d4c6302c96c984b7b0f07
Liech <nicolas_harttig@hotmail.de>
2022-01-29 23:49:35 +0100

    dynamic momentum freeze

--- Assets/Scripts/Ball/MomentumFreeze.cs
+++ Assets/Scripts/Ball/MomentumFreeze.cs
@@ -2,19 +2,28 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(SpeedControl))]
 [RequireComponent(typeof(Rigidbody2D))]
 public class MomentumFreeze : MonoBehaviour {
+  public float minFreezeDuration = 0.1f;
+  public float maxFreezeDuration = 2;
+
   public float freezeDuration = 0.1f;
   public float freezeThreshold = 5;
+  public AnimationCurve freezeDurationFactor;
 
+  private float maxSpeed = float.MaxValue;
   bool frozen = false;
   Vector2 lastVelocity;
   Vector2 newVelocity;
   Rigidbody2D body;
+  private float beefigkeit = 0;
 
   void Start() {
     body=GetComponent<Rigidbody2D>();
     lastVelocity=body.velocity;
+    if(GetComponent<SpeedControl>())
+      maxSpeed = GetComponent<SpeedControl>().MaxSpeed;
   }
   
   // Update is called once per frame
@@ -41,7 +50,10 @@ public class MomentumFreeze : MonoBehaviour {
     lastVelocity=body.velocity;
     body.constraints=RigidbodyConstraints2D.FreezeAll;
 
-    yield return new WaitForSeconds(freezeDuration);
+    float perc = ((newVelocity.magnitude-freezeThreshold) / (maxSpeed-freezeThreshold));
+    beefigkeit=perc;
+    float dynamicFreeze = freezeDurationFactor.Evaluate(perc)*maxFreezeDuration;
+    yield return new WaitForSeconds(minFreezeDuration + dynamicFreeze);
 
     body.constraints=RigidbodyConstraints2D.None;
     frozen=false;
@@ -51,6 +63,6 @@ public class MomentumFreeze : MonoBehaviour {
 
   private void OnDrawGizmos() {
     if(frozen)
-      Gizmos.DrawCube(transform.position, new Vector3(1, 2, 1));
+      Gizmos.DrawCube(transform.position, new Vector3(1, 1, 1) * beefigkeit);
   }
 }
