e74b1318d661e4c630d60ce91cbbe154aef2469a
Kulesko <thegreg@freenet.de>
2022-01-30 11:00:44 +0100

    introduce random player skins and skin swap

---
+++ Assets/CharacterSwapper.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+
+public class CharacterSwapper : MonoBehaviour
+{
+    public GameObject defaultSkin;
+
+    private GameObject _activeSkin;
+    private PlayerBehavior _playerBehavior;
+
+    private void Awake()
+    {
+        _playerBehavior = GetComponent<PlayerBehavior>();
+        _activeSkin = defaultSkin;
+        ChooseRandom();
+    }
+
+    private void OnEnable()
+    {
+        ChooseRandom();
+    }
+
+    private void ChooseRandom()
+    {
+        ChooseCharacter(Random.Range(0, 4));
+    }
+
+    public void ChooseCharacter(int i)
+    {
+        var skin = gameObject.transform.GetChild(i).gameObject;
+        if (!skin.name.StartsWith("SM_")) skin = defaultSkin;
+        if (!skin.activeSelf) SetSkin(skin);
+    }
+
+    private void SetSkin(GameObject skin)
+    {
+        _activeSkin.SetActive(false);
+        _activeSkin = skin;
+        _activeSkin.SetActive(true);
+
+        _playerBehavior.animator = _activeSkin.GetComponent<Animator>();
+    }
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+    }
+}
