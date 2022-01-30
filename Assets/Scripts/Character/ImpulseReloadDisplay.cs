c792f656ce9223c3964fab80c3d61c0e08e6cfc4
Liech <nicolas_harttig@hotmail.de>
2022-01-30 13:40:46 +0100

    dash rloead visualization

--- Assets/Scripts/Character/ImpulseReloadDisplay.cs
+++ Assets/Scripts/Character/ImpulseReloadDisplay.cs
@@ -2,14 +2,14 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-[RequireComponent(typeof(MagnetImpulse))]
+[RequireComponent(typeof(Dash))]
 public class ImpulseReloadDisplay : MonoBehaviour
 {
   public List<Sprite> sprites;
   void Update() {
     var renderer = transform.Find("PlayerColorIndication").GetComponent<SpriteRenderer>();
-    var imp = GetComponent<MagnetImpulse>();
-    float perc = (Time.time-imp.TimeSinceImpulse)/imp.cooldownDuration;
+    var imp = GetComponent<Dash>();
+    float perc = (Time.time-imp.TimeSinceImpulse)/imp.Cooldown;
     int index = (int)((sprites.Count)*perc);
     if(index<0)
       index=0;
