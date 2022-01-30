0f849b2c7b1762f4bf32eb65dc0ec6304e207213
NITROKRAI <62996326+NITROKRAI@users.noreply.github.com>
2022-01-30 15:16:32 +0100

    MainMenu

--- Assets/Scripts/MainMenuManager.cs
+++ Assets/Scripts/MainMenuManager.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class MainMenuManager : MonoBehaviour
 {
@@ -37,6 +38,10 @@ public class MainMenuManager : MonoBehaviour
             menus[i].gameObject.SetActive(false);
         }
         menus[1].SetActive(true);
+    }
+    public void Startgame()
+    {
+        SceneManager.LoadScene("NikiMagneticDevScene");
     }
     public void GameLobby()
     {
